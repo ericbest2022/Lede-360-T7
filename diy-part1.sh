fbaed6fbd31fbb0edef1e3d29ac52a96bdf40fc7
ericbest2022 <wenlong2022@qq.com>
2025-11-17 09:43:51 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,6 +13,5 @@
 # Uncomment a feed source
 
 # Add a feed source
-echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
-echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
+echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main" >> "feeds.conf.default"
 echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
